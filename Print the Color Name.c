86cf4e29e6932c3b71e5fb9c7245d5076dad4dcc
VARSHITHARAJA <rajavarshitha01@gmail.com>
2025-12-09 18:58:00 +0530

    Print the Color Name.c

---
+++ Print the Color Name.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+
+char colour;
+scanf("%c",&colour);
+switch (colour)
+
+case 'V':
+printf("Violet");
+break;
+
+case 'I':
+printf("Indigo");
+break;
+case 'B'
+printf("Blue");
+break;
+ case 'G':
+printf("Green");
+break;
+case 'Y' :
+printf("Yellow");
+break;
+case 'O' : 
+printf("Orange');
+break;
+case 'R' :
+printf("Red");
+break;
+
+
+cdefault : 
+printf("-1");
+return 0;
+}
