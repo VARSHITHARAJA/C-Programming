84f7898ea4f1076da8505131b71b50465d31d1fb
VARSHITHARAJA <rajavarshitha01@gmail.com>
2025-12-09 19:14:12 +0530

    Multiplication.c

---
+++ Multiplication.c
@@ -0,0 +1,13 @@
+#include< stdio.h>
+
+
+int main()
+{
+int num1;
+int num2;
+int multiplication;
+scanf("%d%d",&num1,&num2);
+multiplication=(num1*num2);
+printf("%d",multiplication);
+return 0;
+}
